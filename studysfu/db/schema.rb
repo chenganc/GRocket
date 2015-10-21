78346748bd3cf497094febdbeb8d3682c8dceb78
agassic <agassic@sfu.ca>
2015-10-21 14:49:33 -0700

    Added remember me button

--- studysfu/db/schema.rb
+++ studysfu/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021183755) do
+ActiveRecord::Schema.define(version: 20151021214002) do
 
   create_table "users", force: :cascade do |t|
     t.string   "FirstName"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151021183755) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["Email"], name: "index_users_on_Email", unique: true
