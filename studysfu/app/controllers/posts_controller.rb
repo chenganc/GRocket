61009c9c9cda524add8036ba8eca22e7333c860b
agassic <agassic@sfu.ca>
2015-11-07 16:57:15 -0800

    Update posts_controller.rb

--- studysfu/app/controllers/posts_controller.rb
+++ studysfu/app/controllers/posts_controller.rb
@@ -2,16 +2,18 @@ class PostsController < ApplicationController
 	#before_action :sessions.logged_in?, only: [:create, :upvote, :downvote]
 
 
-	  before_action :logged_in_user, only: [:create, :upvote, :downvote]
+	before_action :logged_in_user, only: [:create, :upvote, :downvote, :destroy]
+	before_action :correct_user,   only: :destroy
+
 	def index
-		@posts = Post.all
+		@posts = Post.all.paginate(page: params[:page])
 	end
 
 	def create
 		@post = current_user.posts.build(post_params)
 
 		if @post.save
-			redirect_to root_path
+			redirect_to posts_path
 		else
 			render posts_path
 		end
@@ -45,6 +47,18 @@ class PostsController < ApplicationController
 		redirect_to posts_path
 	end
 
+#  	def destroy
+#		@post.destroy
+#		flash[:success] = "Micropost deleted"
+#		redirect_to request.referrer || root_url
+#  	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to request.referrer
+	end
+
 	private
 
 		def post_params
@@ -59,4 +73,9 @@ class PostsController < ApplicationController
 				redirect_to login_url
 			end
 		end
+
+	    def correct_user
+	      @post = current_user.posts.find_by(id: params[:id])
+	      redirect_to root_url if @post.nil?
+	    end
 end
