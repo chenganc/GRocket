3188d09a6cefb8fc89296b3237d0dd4571f957ae
agassic <agassic@sfu.ca>
2015-11-10 17:47:47 -0800

    Update comments_controller.rb

--- studysfu/app/controllers/comments_controller.rb
+++ studysfu/app/controllers/comments_controller.rb
@@ -8,18 +8,15 @@ class CommentsController < ApplicationController
     @comment = @link.comments.new(comment_params)
     @comment.user = current_user
 
-    respond_to do |format|
       if @comment.save
         flash[:info] = "Comment was successfully created."
         format.html { redirect_to @link, notice: 'Comment was successfully created.' }
-        format.json { render json: @comment, status: :created, location: @comment }
       else
         flash[:error] = "Operation was unsuccessful."
-        render 'new'
+        redirect_back_or @link
         #format.html { render action: "new" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
-    end
+    
   end
 
   # DELETE /comments/1
@@ -42,7 +39,7 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:link_id, :body, :user_id)
+      params.require(:comment).permit(:body)
     end
 
     def authorized_user
