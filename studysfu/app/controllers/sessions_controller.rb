4bd55a88407e2269d90a34999678247af12d3a61
agassic <agassic@sfu.ca>
2015-11-10 17:47:20 -0800

    Update sessions_controller.rb

--- studysfu/app/controllers/sessions_controller.rb
+++ studysfu/app/controllers/sessions_controller.rb
@@ -11,16 +11,10 @@ class SessionsController < ApplicationController
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_back_or user
       else
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-        redirect_back_or user
-
-        # This is to ignore the email activation 
-        # Uncomment this after and take out stuff above this
-        #message  = "Account not activated. "
-        #message += "Check your email for the activation link."
-        #flash[:error] = message
-        #redirect_to root_url
+        message  = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:error] = message
+        redirect_to root_url
       end
     else
       flash.now[:error] = 'Invalid email/password combination'
