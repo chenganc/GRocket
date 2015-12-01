eb8ca93afb9dc866e8063ce58bd4c982880944ca
Cheng <chengantw@gmail.com>
2015-12-01 03:35:46 -0800

    Fixed event bug

--- studysfu/app/models/event.rb
+++ studysfu/app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :user
   validates :title, presence: true
+  validate :end_must_be_after_start, on: :create
   scope :between, lambda {|start_time, end_time|
     {:conditions => ["? < starts_at < ?", Event.format_date(start_time), Event.format_date(end_time)] }
   }
@@ -24,6 +25,12 @@ class Event < ActiveRecord::Base
 
   def self.format_date(date_time)
     Time.at(date_time.to_i).to_formatted_s(:db)
+  end
+
+  def end_must_be_after_start
+    if starts_at >= ends_at
+      errors.add(:ends_at, "must be after start time")
+    end
   end
 
 end
